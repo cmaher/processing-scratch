16928d83c9bb3e531f012669062672918bd89329
Christian Maher <maher.cs@gmail.com>
2020-03-26 09:05:45 -0400

    Generalize example 1-1 to adapt to canvas size

--- src/processing_scratch/genart/ex1_1.clj
+++ src/processing_scratch/genart/ex1_1.clj
@@ -8,7 +8,6 @@
     :noise-y (q/random 10)))
 
 ; the illusion of depth and shading is created by many overlapping edges
-; the numbers in here were picked for a 2000x2000 image, so most will need to be tweaked when changinyg the screen size
 (defn draw [state]
   (q/background 150)
   (q/stroke 0 50)
@@ -19,6 +18,10 @@
         ynoise (:noise-y state)
         screen-end 8
         step 3
+        ; This is the main value to tweak when changing the canvas size
+        ; Original value is .02 for a 20000-size square.
+        ; assumes canvas is a square.
+        noise-step (/ 40 (q/width))
         ; when centered around 0, -1/8 to 1/8 is 1/4 of the viewable area, before step & noise multiple are factored in
         xend (/ (q/width) screen-end)
         yend (/ (q/height) screen-end)
@@ -28,22 +31,23 @@
         ; steeper noise = more loose. more gradual noise = closer?
         draw-point (fn [x y noise]
                      (q/with-translation
-                       [(* x noise 6) (* y noise 6)]
-                       (let [diameter
-                             (* noise 26)]
-                         (q/ellipse 0, 0, diameter diameter))))
+                       [(* x noise 4) (* y noise 4)]
+                       (let [diameter (* noise 26)]
+                         ; an interesting variation is adding x/y to the diameter given
+                         (q/ellipse 0 0 diameter diameter))))
         ]
     (dorun (for [x (range (count xs))
                  y (range (count ys))]
              (draw-point (xs x) (ys y)
                          ; perlin noise, the x/y modifications here are just advancing along the noise plane
-                         (q/noise (+ xnoise (* x 0.02))
-                                  (+ ynoise (* y 0.02))))))))
+                         ; the noi
+                         (q/noise (+ xnoise (* x noise-step))
+                                  (+ ynoise (* y noise-step))))))))
 
 (defn mouse-pressed [state _event]
   (assoc state
-    :noise-x (q/random 1)
-    :noise-y (q/random 1)))
+    :noise-x (q/random 10)
+    :noise-y (q/random 10)))
 
 (defn ex1-1 []
   (q/defsketch
